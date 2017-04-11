07f38e6cb0e4799d3c4eb9250eb7f0a0114033f2
lucaskup <lucas.kup@gmail.com>
2017-04-11 17:53:19 -0300

    modificado algoritimo de posicionamento

--- gpsMessageFile.py
+++ gpsMessageFile.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 from numpy.linalg import inv
+from numpy.linalg import norm
+
 from copy import deepcopy
 from copy import copy
 class RinexFileReader:
@@ -50,7 +52,7 @@ class RinexFileReader:
                     linhas_pular = int(content[endOfHeaderLineNumber + 1 + lines_visited][30:32])
                     #print(linhas_pular,content[endOfHeaderLineNumber + 1 + lines_visited])
 
-                    gpsData = content[endOfHeaderLineNumber + 1 + lines_visited:endOfHeaderLineNumber  + lines_visited+linhas_pular]
+                    gpsData = content[endOfHeaderLineNumber + 1 + lines_visited:endOfHeaderLineNumber  + lines_visited+linhas_pular+2]
                     lines_visited += linhas_pular+1
                     for i in range(len(gpsData)):
                         gpsData[i] = gpsData[i].replace('D', 'E')
@@ -96,8 +98,9 @@ class RinexFileReader:
 
 class GpsMessageFile:
     def __init__(self):
-        """dtr e o tempo que passou desde o horario importado que consta no arquivo importado"""
-        self.dtr = 0
+        """diffTimeEphObs e o tempo que passou desde o horario importado que consta no arquivo importado"""
+        self.diffTimeEphObs = 0
+
         """PD e a informacao de pseudo distancia que deve ser passada para tornar os calculos de posicao mais precisos"""
         self.PD = 0
         """o atributo coord_mult serve para aplicar um multiplicador 10**coord_mult ao resultado, permitindo variar
@@ -109,14 +112,14 @@ class GpsMessageFile:
         #tr=(0*24+0)*3600+(0*60)+30
         tr = self.toeTime
         #PD = 0
-        #dtr = 0
+        #diffTimeEphObs = 0
         c = 299792458
         tgps = tr - self.PD/c;
 
         dts  = self.sv_clock_bias + self.sv_clock_drift*(tgps - self.toeTime) + self.sv_clock_drift_rate*(tgps-self.toeTime)**2;
 
         #%tempo de propagação aproximado do sinal
-        tal = self.PD/c - self.dtr + dts
+        tal = self.PD/c - self.diffTimeEphObs + dts
 
         tgps = tr - tal + dts;
 
@@ -124,10 +127,13 @@ class GpsMessageFile:
 
         dt = tgps - self.toeTime;
         return dt
+
+    def F(self):
+        return -4.442807633E-10 # Constant, [sec/(meter)^(1/2)]
     def GM(self):
         return 3.986005E+14
     def omegaEarth(self):
-        return 7.292115E-5
+        return 7.292115090E-5
     def n0(self):
         return (self.GM()/(self.sqrtA**6))**0.5
     def n(self):
@@ -145,6 +151,43 @@ class GpsMessageFile:
             lim += 1
             #print('lim',lim,e1)
         return e1
+    """dtr relativistc correction"""
+    def dtr(self):
+        return self.F() * self.eccentricity * self.sqrtA * math.sin(self.E())
+    def Rel(self):
+        return self.dtr()*299792458
+    """Compute satellite clock correction"""
+    def julianDay(self):
+        # Converts the two digit year to a four digit year.
+        # Two digit year represents a year in the range 1980-2079.
+        if self.epochYear >= 80 and self.epochYear <= 99:
+            year = 1900 + self.epochYear;
+
+        if self.epochYear >= 0 and self.epochYear <= 79:
+            year = 2000 + self.epochYear;
+
+        # Calculates the 'm' term used below from the given calendar month.
+        if self.epochMonth <= 2:
+            y = year - 1;
+            m = self.epochMonth + 12;
+        if self.epochMonth > 2:
+            y = year
+            m = self.epochMonth
+
+        #Computes the Julian date corresponding to the given calendar date.
+        return  round((365.25 * y),0) + round((30.6001 * (m+1)),0) + self.epochDay + ( (self.epochHour + self.epochMinute / 60 + self.epochSecond / 3600) / 24 ) + 1720981.5
+    def gpsWeek2(self):
+        return round( (self.julianDay() - 2444244.5) / 7 ,0)
+    def gpsSeconds(self):
+        secs_per_week = 604800
+        return round(((((self.julianDay()-2444244.5) / 7)-self.gpsWeek2())*secs_per_week)/0.5,0)*0.5
+    def satClkCorr(self):
+
+
+
+        clkCorr = (self.sv_clock_drift_rate * (self.diffTimeEphObs) + self.sv_clock_drift) * (self.diffTimeEphObs) + self.sv_clock_bias
+
+        return clkCorr*299792458
     #True Anomaly
     def V(self):
         senv = (((1-self.eccentricity**2)**0.5)*math.sin(self.E()))/(1-self.eccentricity*math.cos(self.E()))
@@ -230,38 +273,83 @@ class GPSReceptor:
             if len(lista) > 0:
                 lista = sorted(lista,key=lambda gps: abs(self.epochYear - gps.epochYear)*365*24*60*60 + abs(self.epochMonth - gps.epochMonth)*30*24*60*60 + abs(self.epochDay - gps.epochDay)*24*60*60 + abs(self.epochHour - gps.epochHour)*60*60 + abs(self.epochMinute - gps.epochMinute)*60 + abs(self.epochSecond - gps.epochSecond))
                 g = copy(lista[0])
-                g.dtr = (self.epochYear - g.epochYear)*365*24*60*60 + (self.epochMonth - g.epochMonth)*30*24*60*60 + (self.epochDay - g.epochDay)*24*60*60 + (self.epochHour - g.epochHour)*60*60 + (self.epochMinute - g.epochMinute)*60 + (self.epochSecond - g.epochSecond)
-                #print('dtr',g.dtr)
+                g.CorrP1 = self.sat_obs_data[g.sat_number]['P3'] + g.satClkCorr() + g.Rel()
+                g.diffTimeEphObs = (self.epochYear - g.epochYear)*365*24*60*60 + (self.epochMonth - g.epochMonth)*30*24*60*60 + (self.epochDay - g.epochDay)*24*60*60 + (self.epochHour - g.epochHour)*60*60 + (self.epochMinute - g.epochMinute)*60 + (self.epochSecond - g.epochSecond)
+                #print('diffTimeEphObs',g.diffTimeEphObs)
                 g.PD = self.sat_pseudo[g.sat_number]
                 self.gps.append(g)
                 #pseudodistancia
     def getCoordinates(self):
         if len(self.gps) <= 0:
             return None
+        lista_spos = []
         lista_a = []
-        lista_L = []
+        lista_b =  []
+        corrP1 = []
         for gps in self.gps:
 
-            #print('gps',gps.sat_number,str(gps.epochHour),str(gps.epochMinute),str(gps.epochSecond),'dtr',gps.dtr)
+            #print('gps',gps.sat_number,str(gps.epochHour),str(gps.epochMinute),str(gps.epochSecond),'diffTimeEphObs',gps.diffTimeEphObs)
             gps_coord = gps.coordinate_WGS84()
-            #print(gps_coord)
-            denominator = ((self.aprox_pos[0] - gps_coord[0])**2 + (self.aprox_pos[1] - gps_coord[1])**2 + (self.aprox_pos[2] - gps_coord[2])**2)**0.5
-            lista_a.append([(self.aprox_pos[0] - gps_coord[0])/denominator,
-                            (self.aprox_pos[1] - gps_coord[1])/denominator,
-                            (self.aprox_pos[2] - gps_coord[2])/denominator,
-                            1])
-            lista_L.append(self.sat_pseudo[gps.sat_number])
-        matrix_L = np.matrix(lista_L).transpose()
-        matriz_a = np.matrix(lista_a)
-        matriz_aT = matriz_a.transpose()
-        matrix_deltaX = np.dot(inv(np.dot(matriz_aT,matriz_a)),
-                                np.dot(matriz_aT,matrix_L))
+            lista_spos.append([gps_coord[0],gps_coord[1],gps_coord[2]])
+            corrP1.append(gps.CorrP1)
+            lista_b.append(0)
+
+        matrix_b = np.matrix(lista_b)
+
+        matrix_spos = np.matrix(lista_spos)
+
+        rec_xyz = np.matrix([self.aprox_pos[0],self.aprox_pos[1],self.aprox_pos[2]])
+        #for j in range(10):
+        for i in range(len(self.gps)):
+
+            matrix_b[0,i] = (corrP1[i] - norm(matrix_spos[i] - rec_xyz[0] ,ord='fro'))
+            lista_a.append([(-(matrix_spos[i,0] - rec_xyz[0,0])) / corrP1[i] ,
+                            (-(matrix_spos[i,1] - rec_xyz[0,1])) / corrP1[i] ,
+                            (-(matrix_spos[i,2] - rec_xyz[0,2])) / corrP1[i] ,
+                            1 ])
+        matrix_a = np.matrix(lista_a)
+        delta_xyzb = np.linalg.lstsq(matrix_a,matrix_b.transpose())[0]
+        rec_xyz = rec_xyz - delta_xyzb[:-1].transpose()
+        lista_a = []
+        return  (rec_xyz[0,0],rec_xyz[0,1],rec_xyz[0,2])
+    def getCoordinates_(self):
+        if len(self.gps) <= 0:
+            return None
+
         lista_X0 = [self.aprox_pos[0],self.aprox_pos[1],self.aprox_pos[2],0]
+        #lista_X0 = [0,0,0,0]
         matrix_X0 = np.matrix(lista_X0).transpose()
-        matrix_Xa = matrix_X0 + matrix_deltaX
-        #print('mariz deltaX',matrix_deltaX)
-        #print('mariz X0',matrix_X0)
-        #print('mariz Xa',matrix_Xa)
+
+        for i in range(1):
+            lista_a = []
+            lista_L = []
+
+            for gps in self.gps:
+
+                #print('gps',gps.sat_number,str(gps.epochHour),str(gps.epochMinute),str(gps.epochSecond),'diffTimeEphObs',gps.diffTimeEphObs)
+                gps_coord = gps.coordinate_WGS84()
+                #print(gps_coord)
+                denominator = ((lista_X0[0] - gps_coord[0])**2 + (lista_X0[1] - gps_coord[1])**2 + (lista_X0[2] - gps_coord[2])**2)**0.5
+                lista_a.append([(lista_X0[0] - gps_coord[0])/denominator,
+                                (lista_X0[1] - gps_coord[1])/denominator,
+                                (lista_X0[2] - gps_coord[2])/denominator,
+                                1])
+                lista_L.append(self.sat_pseudo[gps.sat_number])
+            matrix_L = np.matrix(lista_L).transpose()
+            matriz_a = np.matrix(lista_a)
+            matriz_aT = matriz_a.transpose()
+            matrix_deltaX = np.dot(inv(np.dot(matriz_aT,matriz_a)),
+                                    np.dot(matriz_aT,matrix_L))
+
+            #matrix_deltaX[0][0] = 90
+            matrix_Xa = matrix_X0 + matrix_deltaX
+
+            print('mariz deltaX',matrix_deltaX)
+            print('mariz X0',matrix_X0)
+            print('mariz Xa',matrix_Xa)
+
+            lista_X0 = [matrix_Xa.A[0][0],matrix_Xa.A[1][0],matrix_Xa.A[2][0],matrix_Xa.A[3][0]]
+            matrix_X0 = np.matrix(lista_X0).transpose()
         return (matrix_Xa.A[0][0],matrix_Xa.A[1][0],matrix_Xa.A[2][0])
 
 
@@ -272,15 +360,33 @@ class GPSFactory:
             g = GPSFactory._receptorFromRinex2(fileBlock, aprox_pos, sat_pseudo)
         return g
     def _receptorFromRinex2(fileBlock,aprox_pos, sat_pseudo):
+
+        fL1 = 1575.42E6   # L1 frequency (Hz)
+        fL2 = 1227.6E6    # L2 frequency (Hz)
+        B=fL2**2/(fL2**2-fL1**2)
+        A=-B+1;
+
+
         r = GPSReceptor()
         r.aprox_pos = aprox_pos
         r.sat_number = []
         r.gps = []
         r.sat_pseudo = sat_pseudo
+        r.sat_obs_data = {}
         auxString = fileBlock[0][31:].strip()
         auxSats = auxString.split('G')
         for i in range(1,len(auxSats)):
             r.sat_number.append(auxSats[i].strip())
+            c1 = float(fileBlock[i][2:14].strip())
+            l1 = float(fileBlock[i][17:32].strip())
+            p2 = float(fileBlock[i][34:46].strip())
+            l2 = float(fileBlock[i][50:].strip())
+
+            p3=A*c1+B*p2
+
+
+            r.sat_obs_data[auxSats[i].strip()] = {'C1':c1,'L1':l1,'P2':p2,'L2':l2,'P3':p3}
+        print(r.sat_obs_data)
         r.epochYear = int(fileBlock[0][0:3])
         r.epochMonth = int(fileBlock[0][4:6])
         r.epochDay = int(fileBlock[0][8:10])
